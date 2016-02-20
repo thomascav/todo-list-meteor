0b874a514abf6dc18662dcc84351b4c2f896c9ba
thomascav <tcavalar@numericable.fr>
2016-02-20 17:35:15 +0100

    added form for new tasks, added delete functionnality => html&js

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,22 +1,27 @@
-
 // define template content, called in html file by template tag
 if (Meteor.isClient) {
 
-  // This code only runs on the client
+    // This code only runs on the client
 
-  Template.body.helpers({
+    Template.body.helpers({
 
-    tasks: [
+        tasks: [
 
-      { text: "This is task 1" },
+            {
+                text: "This is task 1"
+            },
 
-      { text: "This is task 2" },
+            {
+                text: "This is task 2"
+            },
 
-      { text: "This is task 3" }
+            {
+                text: "This is task 3"
+            }
 
-    ]
+        ]
 
-  });
+    });
 
 }
 
@@ -27,16 +32,84 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
 
-  // This code only runs on the client
+    // This code only runs on the client
 
-  Template.body.helpers({
+    Template.body.helpers({
 
-    tasks: function () {
+        tasks: function() {
 
-      return Tasks.find({});
+            // Show newest tasks at the top
 
-    }
+            return Tasks.find({}, {
+                sort: {
+                    createdAt: -1
+                }
+            });
 
-  });
+        }
+
+    });
+
+
+
+    Template.body.events({
+        // event => add a new task by a form
+        "submit .new-task": function(event) {
+
+            // Prevent default browser form submit
+
+            event.preventDefault();
+
+
+
+            // Get value from form element
+
+            var text = event.target.text.value;
+
+
+
+            // Insert a task into the collection
+
+            Tasks.insert({
+
+                text: text,
+
+                createdAt: new Date() // current time
+
+            });
+            console.log(event);
+
+
+            // Clear form
+
+            event.target.text.value = "";
+            // end of the event
+        }
+
+    });
+
+    Template.task.events({
+
+        "click .toggle-checked": function() {
+
+            // Set the checked property to the opposite of its current value
+
+            Tasks.update(this._id, {
+
+                $set: {
+                    checked: !this.checked
+                }
+
+            });
+
+        },
+// Delete functionnality for checked elements
+        "click .delete": function() {
+
+            Tasks.remove(this._id);
+
+        }
+
+    });
 
 }
